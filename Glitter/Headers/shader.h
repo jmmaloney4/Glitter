4250b76c914f4a965f63677832ccb145986f8bc7
Jack Maloney <jmmaloney4@gmail.com>
2016-08-14 17:40:24 -0500

    License Headers

---
+++ Glitter/Headers/shader.h
@@ -0,0 +1,15 @@
+// Oxygen - shader.h
+//
+// Copyright (C) Jack Maloney 2016
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Based off Glitter. https://github.com/Polytonic/Glitter
+
+#ifndef OXYGEN_SHADER_H
+#define OXYGEN_SHADER_H
+#pragma once
+
+#endif
